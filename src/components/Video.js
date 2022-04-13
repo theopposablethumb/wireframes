b4e68a5ab0f95d6e5ea26d238333c91599dce031
theopposablethumb <brendan.meachen@gmail.com>
2022-04-13 11:02:33 +0100

    Changed video to something more appropriate

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,6 +1,18 @@
 const Video = ({ width, height }) => {
   return (
-    <iframe width={width} height={height} src="https://www.youtube.com/embed/7wPJkSmHlCI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/9ZfN87gSjvI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    // <div
+    //   style={{ position: 'relative', display: 'block', maxWidth: '960px' }}>
+    //   <div
+    //     style={{ paddingTop: '56.25%'}}>
+    //     <iframe
+    //       title="brightcove video"
+    //       src="https://players.brightcove.net/pages/v1/index.html?accountId=3871979161001&playerId=DnkKsSZvR&videoId=6303595772001&bccumstomguid=NA&bctrackingid=GB"
+    //       allowfullscreen=""
+    //       allow="encrypted-media"
+    //       style={{position: 'absolute', top: '0px', right: '0px', bottom: '0px', left: '0px', width: '100%', height: '100%' }}></iframe>
+    //   </div>
+    // </div>
   )
 }
 
